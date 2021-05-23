8f48b519a4d9c97c63b41bfdfeb4006cc8a499de
haluskua <haluskua.one@gmail.com>
2021-05-24 00:53:21 +1200

    added sidDrawer component, with clickable button on NavBAR

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,57 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Toolbar from "@material-ui/core/Toolbar";
 import AppBar from "@material-ui/core/AppBar";
-// import ListItem from "@material-ui/core/ListItem";
-// import ListItemIcon from "@material-ui/core/ListItemIcon";
-// import ListItemText from "@material-ui/core/ListItemText";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import AssignmentInd from "@material-ui/icons/AssignmentInd";
+import ArrowBack from "@material-ui/icons/ArrowBack";
+import Apps from "@material-ui/icons/Apps";
+import ContactMail from "@material-ui/icons/ContactMail";
+import IconButton from "@material-ui/core/IconButton";
+import Avatar from "@material-ui/core/Avatar";
+import avatar from "../avatar.png";
 import { makeStyles } from "@material-ui/core/styles";
-// import Apps from "@material-ui/core/Apps";
-// import Home from "@material-ui/core/Home";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Home from "@material-ui/icons/Home";
 import Footer from "../components/Footer";
-
-
 
 
 const useStyles = makeStyles((theme) => ({
     appbar: {
-        background: "#171b1f",
+        background: "#222",
         margin: 0,
-        border: "5px"
+    },
+    arrow: {
+        color: "tomato",
+    },
+    title: {
+        color: "tan",
     },
     menuSliderContainer: {
-        width: 22250,
+        width: 250,
         background: "#511",
         height: "100%",
     },
-
-    listText: {
+    avatar: {
+        display: "block",
+        margin: "0.5rem auto",
+        width: theme.spacing(13),
+        height: theme.spacing(13),
+    },
+    listItem: {
         color: "tan",
-    }
+    },
 }));
 
 const menuItems = [
-    // { listIcon: <Home />, listText: "Home", listPath: "/" },
-    // { listIcon: <Resume />, listText: "Resume", listPath: "/resume" },
-    // { listIcon: <Apps />, listText: "Portfolio", listPath: "/portfolio" },
-    // { listIcon: <ContactMail />, listText: "Contact", listPath: "/contact" },
+    { listIcon: <Home />, listText: "Home", listPath: "/" },
+    { listIcon: <AssignmentInd />, listText: "Resume", listPath: "/resume" },
+    { listIcon: <Apps />, listText: "Portfolio", listPath: "/portfolio" },
+    { listIcon: <ContactMail />, listText: "Contact", listPath: "/contact" },
 ];
 
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
     const classes = useStyles();
+
+    // Creating the functionality for SIDE-PANEL
+
+    const sideList = () => (
+        <Box component="nav">
+            <AppBar position="static" className={classes.appbar}>
+                <Toolbar>
+                    <IconButton onClick={() => setOpen(true)}>
+                        <ArrowBack className={classes.arrow} />
+                    </IconButton>
+                    <Typography variant="h5" className={classes.title}>
+                        Portfolio
+            </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 
     return (
         <React.Fragment>
 
             <Box component="nav">
                 <AppBar position="static" className={classes.appbar}>
-                    <Typography variant="h5" className={classes.title}>
-                        Portfolio
+                    <Toolbar>
+                        <IconButton onClick={() => setOpen(true)}>
+                            <ArrowBack className={classes.arrow} />
+                        </IconButton>
+                        <Typography variant="h5" className={classes.title}>
+                            Portfolio
                     </Typography>
+                    </Toolbar>
                 </AppBar>
             </Box>
-            <Footer />
+            <Drawer open={open} anchor="right" onClose={() => setOpen(false)}>
+                {sideList()}
+                <Footer />
+            </Drawer>
         </React.Fragment>
     );
 };
