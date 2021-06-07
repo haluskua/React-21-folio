357022523e01149bf432574cbe1a3a67620aa98d
haluskua <haluskua.one@gmail.com>
2021-06-07 19:48:09 +1200

    fixed build issue, Header is defined but never used

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header';
+// import Header from './Header';
 import Particles from './Particles';
 
 import FrontCard from '../components/3D/index';
