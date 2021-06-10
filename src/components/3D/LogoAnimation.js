cfd22e2e31450b9ad7def9e5527e691e41ed590d
haluskua <haluskua.one@gmail.com>
2021-06-10 23:55:34 +1200

    fixed minor Netlify error

--- src/components/3D/LogoAnimation.js
+++ src/components/3D/LogoAnimation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from "../../../src/avatar.png";
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 export default function LogoAnimation() {
 
@@ -10,30 +10,6 @@ export default function LogoAnimation() {
         </AniLogo>
     );
 }
-
-const animation = keyframes`
- 0% {
-     opacity: 0.05; 
-     transform: translateY(-75px) skewY(10deg) skewX(10deg) rotateZ(10deg);
-     filter: blur(10px);
-     }
- 20% { 
-      opacity: 1; 
-      transform: translateY(0px) skewY(0deg) skewX(0deg) rotateZ(0deg);
-    filter: blur(0px) }
- 85% { 
-      opacity: 1; 
-      transform: translateY(0px) skewY(0deg) skewX(0deg) rotateZ(0deg);
-      filter: blur(0px) }
- 97% { 
-       opacity: 0.41; 
-       transform: translateY(0px) skewY(0deg) skewX(0deg) rotateZ(0deg);
-       filter: blur(5px) }
- 100% { 
-       opacity: 0; 
-       transform: translateY(-75px) skewY(10deg) skewX(10deg) rotateZ(30deg);
-       filter: blur(10px) }
-`;
 
 
 const AniLogo = styled.div`
