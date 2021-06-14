6cd9e2202862ce67c51add0a5de261a8a88ae256
haluskua <haluskua.one@gmail.com>
2021-06-14 23:30:55 +1200

    fixed another netlify error suggestion

--- src/components/3D/logoDetails.js
+++ src/components/3D/logoDetails.js
@@ -130,15 +130,3 @@ const SpacedHorizontalContainer = styled.div`
   align-items: flex-end;
 `;
 
-const Logo = styled.div`
-  width: 100%;
-  height: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  
-  img {
-      width: auto;
-      height: 48px;
-  }
-`;
