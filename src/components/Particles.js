5ea8ab7f8abad2dc106911e8038a78f49d071156
haluskua <haluskua.one@gmail.com>
2021-08-07 12:18:18 +1200

    centered profile image on Particles.js

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -88,7 +88,8 @@ const useStyles = makeStyles((theme) => ({
   img_box: {
     // boxShadow: "3px 3px 6px #cecfd4, -3px -3px 6px #fff",
     flexDirection: "column",
-    borderRadius: "12.2px",
+    borderRadius: "12.2px",align
+    alignItem: "center",
     padding: ".5rem",
   },
   // particlesCanvas: {
