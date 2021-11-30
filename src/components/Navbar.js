682c9fdb6d6521a388b37ffbb9c34c2d6cbc2c16
haluskua <haluskua.one@gmail.com>
2021-11-30 14:15:45 +1300

    bug fix test netlify 4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,24 +52,27 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     flexDirection: "column",
     alignItems: "center",
-    background: "#0d1625",
     borderRadius: "2rem 2rem 3rem 3rem !important",
     boxShadow: "inset 2px 2px 5px #11243c, inset -3px -3px 7px #061628",
     //background
-  background:"-moz-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
+    background:
+      "-moz-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
 
-  /* safari 5.1+,chrome 10+ */
-  background:"-webkit-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
+    /* safari 5.1+,chrome 10+ */
+    background:
+      "-webkit-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
 
-  /* opera 11.10+ */
-  background:"-o-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
+    /* opera 11.10+ */
+    background:
+      "-o-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
 
-  /* ie 10+ */
-  background:"-ms-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
+    /* ie 10+ */
+    background:
+      "-ms-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
 
-  /* global 92%+ browsers support */
-  background:"radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
-
+    /* global 92%+ browsers support */
+    background:
+      "radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
 
     //Portrait View Mqueries
     [theme.breakpoints.up("xs")]: {
@@ -203,7 +206,11 @@ const Navbar = () => {
   const classes = useStyles();
 
   const sideList = () => (
-    <Box className={classes.menuSliderContainer} id="menuSliderContainer" component="div">
+    <Box
+      className={classes.menuSliderContainer}
+      id="menuSliderContainer"
+      component="div"
+    >
       <Avatar className={classes.avatar} src={avatar} alt="Ompa" />
       <NameAnimation />
       <Divider className={classes.divider_one} />
