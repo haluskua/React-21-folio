47d249ee7056b5a3cfa86101ac53e2ec45245c13
haluskua <haluskua.one@gmail.com>
2021-11-30 14:21:10 +1300

    bug fix test netlify 5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -54,25 +54,6 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     borderRadius: "2rem 2rem 3rem 3rem !important",
     boxShadow: "inset 2px 2px 5px #11243c, inset -3px -3px 7px #061628",
-    //background
-    background:
-      "-moz-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
-
-    /* safari 5.1+,chrome 10+ */
-    background:
-      "-webkit-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
-
-    /* opera 11.10+ */
-    background:
-      "-o-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
-
-    /* ie 10+ */
-    background:
-      "-ms-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
-
-    /* global 92%+ browsers support */
-    background:
-      "radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)",
 
     //Portrait View Mqueries
     [theme.breakpoints.up("xs")]: {
