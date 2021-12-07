a4d9f02e04354c7d3fb0309aa160ff1a180b7079
haluskua <haluskua.one@gmail.com>
2021-12-07 20:38:35 +1300

    added change to my bio

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,21 +7,31 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import ReadMore from "./ReadMore";
-import Animista, {AnimistaTypes} from 'react-animista';
-
+import Animista, { AnimistaTypes } from "react-animista";
 
 // Data
 const backgrounds = [
   {
-    bios: `My name is Omphalus Harkie Kua, and I come from the Highlands of
-    Papua New Guinea. My favorite sport is Rugby League and I was
-    blessed to be given the opportunity represent my country to play an
-    international test against the Australian Kangaroos.
-    I enjoy spending time with my family and friends. Recently I
-    have been very fascinated with the web and the languages that make
-    the web. I appreciate the creative aspects in life, whether it be in
-    sports/outdoors, board-games or cooking to music and what I enjoy
-    most which is in Design and Coding." `,
+    bios: `My name is Omphalus Harkie Kua. I am a freelance Web developer 
+    and Graphics designer. My goal is to one day own my own business in the creative industry 
+    like a media agency focussed on keeping the culture and heritage of the people of 
+    Papua New Guinea from being lost completely.  
+    I have always been very fascinated with the web and the languages that make
+    the web. My area of expertise is in the frontend development, but not without the UX-UI design process and method.
+    My background in Graphic design directs most of my creativity in the realm of a frontend developer.
+    I appreciate the creative aspects in life, whether it be in
+    sports/outdoors, board-games, cooking to music yet nothing beats my interest in 
+    Design and Coding. I know of this because I am constantly trying to stay on top of the 
+    tech stack that I am involved in.
+    My greatest achievement was becoming a father to my son who is now 18years old. I would also consider
+    an international League test with the Kangaroos as another great achievement. Also at 14 I won a
+    art competition in my age group in my country( PNG ).
+    
+    All my friends call me Ompa. I am a chill easy going guy that loves to laugh and have a good time with
+    friends and family. I play the guitar and enjoy painting as my two main hobbies. 
+    I live with my Son, nephew partner and our new born daughter in Wellington.
+     
+ `,
     github: `https://github.com/haluskua/crwn-clothing`,
     site: `https://gracious-kilby-61782e.netlify.app/`,
   },
@@ -32,42 +42,46 @@ export default function MediaCard() {
 
   return (
     <Card className={classes.root}>
-     <Animista
-          type={AnimistaTypes.SCALE_UP_BR}
-          duration="0.851s"
-          style={{
-            width: '100%',
-            height: "auto",
-          }}
-        >
-      {backgrounds.map((text, i) => (
-        <Grid item xs={12} sm={8} md={12}  key={i}>
-          <Card className={classes.cardContainer}>
-            <CardActionArea>
-              <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="h5" component="h2" className={classes.title}>
-                  About Me
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  className={classes.bio}
-                >
-                  <Typography>
-                    <ReadMore>{text.bios}</ReadMore>
+      <Animista
+        type={AnimistaTypes.SCALE_UP_BR}
+        duration="0.851s"
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+      >
+        {backgrounds.map((text, i) => (
+          <Grid item xs={12} sm={8} md={12} key={i}>
+            <Card className={classes.cardContainer}>
+              <CardActionArea>
+                <CardContent className={classes.cardContent}>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="h2"
+                    className={classes.title}
+                  >
+                    About Me
                   </Typography>
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      ))}
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    className={classes.bio}
+                  >
+                    <Typography>
+                      <ReadMore>{text.bios}</ReadMore>
+                    </Typography>
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
       </Animista>
     </Card>
   );
 }
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -108,8 +122,8 @@ const useStyles = makeStyles((theme) => ({
       maxWidth: "88% ",
     },
   },
-  title:{
-    color: "#85b9f9",
+  title: {
+    color: "#66835c",
   },
 
   cardContent: {
