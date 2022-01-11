69558aebdd22812f2e15d59c88f69e3e081f0952
haluskua <haluskua.one@gmail.com>
2022-01-11 15:23:08 +1300

    added original save back

--- api/server.js
+++ api/server.js
@@ -4,18 +4,11 @@ const cors = require("cors");
 const { json } = require("body-parser");
 const axios = require("axios");
 
-const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(json());
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 
 const { parsed: config } = dotenv.config();
