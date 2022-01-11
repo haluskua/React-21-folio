417aed1f4ca7e74150e1e275da9c46f511c61de7
haluskua <haluskua.one@gmail.com>
2022-01-12 01:13:06 +1300

    netlify-build

--- api/server.js
+++ api/server.js
@@ -4,14 +4,22 @@ const cors = require("cors");
 const { json } = require("body-parser");
 const axios = require("axios");
 
+const serverless = require('serverless-http')
+
+
 
 const app = express();
+
+
+
 
 app.use(cors());
 app.use(json());
 
+const router = express.Router();
 
 const { parsed: config } = dotenv.config();
+
 const BASE_URL =`https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/image`
 const auth = { 
               username: config.API_KEY,
@@ -19,7 +27,7 @@ const auth = {
               
                }
 
-app.get("/photos", async (req, res) => {
+router.get("/photos", async (req, res) => {
   const response = await axios.get(BASE_URL, {
     auth, 
     params: {
@@ -29,6 +37,19 @@ app.get("/photos", async (req, res) => {
   return res.send(response.data);
 });
 
+
+router.get('/test', (req, res) => {
+  res.json({
+    "hello": 'test'
+  })
+})
+
+app.use('/.netlify/functions/api', router);
 const PORT = 7000;
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+
+
+
+
+module.exports.handler = serverless(app)
